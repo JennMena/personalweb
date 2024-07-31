54645d8794fdd2756398aed31bdeb0a6946036a4
Jennifer Mena <jmena@negocioefectivo.com>
2024-07-31 02:13:25 -0500

    Modified analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,27 @@
-
-
-import { ToastContainer } from 'react-toastify'
-import '../public/main.scss'
-
-
-
+"use client";
+import React, { useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { Helmet } from 'react-helmet';
+import '../public/main.scss';
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-TG2N8BB5BV');
+  }, []);
+
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TG2N8BB5BV"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-TG2N8BB5BV');
-        </script>
+        <Helmet>
+          <link rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@300;400;500;600;700;800;900&display=swap" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-TG2N8BB5BV"></script>
+        </Helmet>
       </head>
-      <body className='' >
+      <body className=''>
         <ToastContainer
           position='top-right'
           autoClose={2000}
@@ -32,12 +32,9 @@ export default function RootLayout({ children }) {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-
         />
         {children}
-
-
       </body>
     </html>
-  )
+  );
 }
