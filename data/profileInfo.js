bd05b440664518e87adf92b60cfb34b2eb1c3d80
Jennifer M. <133239540+JennMena@users.noreply.github.com>
2024-08-07 22:44:46 -0500

    Update profileInfo.js

--- data/profileInfo.js
+++ data/profileInfo.js
@@ -1,6 +1,6 @@
 export const profileInfo = {
    name:'Jennifer Mena',
-   title:'Software Engineer Student',
+   title:'Software Engineering Student',
    imageSrcOne:'/assets/img/parsonal-info/Headshot-1.1.jpg',
    imageSrcTwo:'/assets/img/parsonal-info/Headshot-1.1.jpg',
    imageSrcThree:'/assets/img/parsonal-info/Headshot-1.1.jpg',
